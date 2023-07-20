1986548f9bf42a5ad2aad6fef539baf4bc7a2f95
coentmn <117341310+coentmn@users.noreply.github.com>
2023-07-20 10:16:55 +0800

    Update 088_m2014 q4b.v

--- 2/088_m2014 q4b.v
+++ 2/088_m2014 q4b.v
@@ -1,7 +1,7 @@
 module top_module (
     input clk,
     input d, 
-    input ar,   // asynchronous reset
+    input ar,   // asynchronous reset-异步复位
     output q);
 
     always @(posedge clk or posedge ar) begin
